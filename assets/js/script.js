817df0c1c64a047999cf17df4d81a86224f1ae13
Sifath <info.mdsifath70@gmail.com>
2022-08-18 03:58:23 +0600

    car

---
+++ assets/js/script.js
@@ -0,0 +1,34 @@
+// header
+const header = document.querySelector('.header');
+window.addEventListener('scroll', function () {
+    if (this.scrollY > 50) {
+        header.classList.add('scroll');
+    } else {
+        header.classList.remove('scroll');
+    }
+});
+
+// menu toggle
+const navMenuBtn = document.querySelector('.nav__menu-btn');
+const navMenuTok = document.querySelector('.header nav .nav__tok');
+navMenuBtn.addEventListener('click', function () {
+    navMenuTok.classList.toggle('show');
+});
+
+// swiper
+function swiperSlider() {
+    const swiper = new Swiper('.swiper', {
+        loop: true,
+        spaceBetween: 15,
+        autoplay: true,
+        pagination: {
+            el: '.swiper-pagination',
+            type: 'fraction',
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+    });
+}
+swiperSlider();
